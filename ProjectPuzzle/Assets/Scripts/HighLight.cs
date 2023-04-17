fe9220d5938cb2e8ada7893e38e488e4e8d673ec
samybaby25 <joguinhosdasamy@gmail.com>
2023-04-17 20:24:15 -0300

    high

--- ProjectPuzzle/Assets/Scripts/HighLight.cs
+++ ProjectPuzzle/Assets/Scripts/HighLight.cs
@@ -1,31 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class HighLight : MonoBehaviour
+public class Highlight : MonoBehaviour
 {
-    public Material playerHighlightMaterial;
-    public Material IAHighlightMaterial;
+    private static Highlight instance;
 
+    // Material usado para destacar o objeto
+    public Material highlightMaterial;
+
+    // Salva o material original do objeto
     private Material originalMaterial;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            originalMaterial = GetComponent<MeshRenderer>().material;
-            GetComponent<MeshRenderer>().material = playerHighlightMaterial;
+    // Armazena o objeto atualmente em destaque
+    private GameObject currentHighlightedObject;
+
+    // Obtém a instância única do script
+    public static Highlight GetInstance() {
+        if (instance == null) {
+            instance = new Highlight();
         }
-        else if (other.CompareTag("Enemy"))
-        {
-            originalMaterial = GetComponent<MeshRenderer>().material;
-            GetComponent<MeshRenderer>().material = IAHighlightMaterial;
-        }
+        return instance;
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        GetComponent<MeshRenderer>().material = originalMaterial;
+    // Destaca o objeto especificado
+    public void HighlightObject(GameObject objectToHighlight) {
+        // Remove o highlight do objeto anterior
+        RemoveHighlight();
+
+        // Armazena o objeto atualmente em destaque
+        currentHighlightedObject = objectToHighlight;
+
+        // Salva o material original do objeto
+        originalMaterial = objectToHighlight.GetComponent<Renderer>().material;
+
+        // Define o material de highlight no objeto
+        objectToHighlight.GetComponent<Renderer>().material = highlightMaterial;
+    }
+
+    // Remove o highlight do objeto atualmente em destaque
+    public void RemoveHighlight() {
+        if (currentHighlightedObject != null) {
+            // Restaura o material original do objeto
+            currentHighlightedObject.GetComponent<Renderer>().material = originalMaterial;
+
+            // Limpa a referência do objeto atualmente em destaque
+            currentHighlightedObject = null;
+        }
     }
 }
-
