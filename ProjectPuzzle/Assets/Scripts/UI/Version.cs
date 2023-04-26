b4f49810a940f4d2c7b57abb10f4a3199e993f4d
hots3 <hots3d@gmail.com>
2023-04-26 20:38:00 -0300

    #FinalCommit v0.2.0-prealpha

--- ProjectPuzzle/Assets/Scripts/UI/Version.cs
+++ ProjectPuzzle/Assets/Scripts/UI/Version.cs
@@ -1,17 +1,1 @@
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.UI;
 
-namespace UI
-{
-    public class Version : MonoBehaviour
-    {
-        void Start()
-        {
-            string version = PlayerSettings.bundleVersion;
-            Text versionText = GetComponentInChildren<Text>();
-            versionText.text = version;
-        }
-    
-    }
-}
